56c8f5d0f9c40a87d97a708ce60d0292e5aff765
jpbrazan <111703474+jpbrazan@users.noreply.github.com>
2022-08-21 13:37:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
-from gsheetsdb import connect
 
 st.title("My First Streamlit Web App")
 
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
-
